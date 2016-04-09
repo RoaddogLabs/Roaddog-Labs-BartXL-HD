d2d525b8ada1fdd4b52e3a6372529fdc6497821c
RoaddogLabs <dave@lebowski.local>
2016-04-08 22:37:28 -0700

    config settings
    adjust bed probe distance
    disable bed runaway

--- BartProXL firmware /Bart_Pro_XL/Configuration.h
+++ BartProXL firmware /Bart_Pro_XL/Configuration.h
@@ -309,7 +309,7 @@ Here are some standard links for getting your machine calibrated:
  */
 
 #define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
-#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
+//#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 
 //===========================================================================
 //============================= Mechanical Settings =========================
