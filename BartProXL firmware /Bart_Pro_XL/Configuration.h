ab5f9aa583ca8877d52701aa50c63074cffd5ad2
RoaddogLabs <dave@lebowski.local>
2016-04-17 18:48:23 -0700

    remove thermal settings

--- BartProXL firmware /Bart_Pro_XL/Configuration.h
+++ BartProXL firmware /Bart_Pro_XL/Configuration.h
@@ -308,7 +308,7 @@ Here are some standard links for getting your machine calibrated:
  * the firmware will halt as a safety precaution.
  */
 
-#define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
+//#define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
 //#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 
 //===========================================================================
